5df9f3aa828c82bb8964fd28969288cb2ca13455
Pablo Gutiérrez <be.legal.copy.linux@gmail.com>
2014-08-14 14:33:52 +0200

    Github project added to Visual Studio 2013 solution

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sogeti.SharePoint.SDK
+{
+    public class Class1
+    {
+    }
+}
